2c968d5bc4ffb4a27a9f353e3d3b496595f0a9fc
Brandon Huyck <bhuyck@pm.me>
2023-11-10 12:23:26 -0800

    added prim and forgn keys

--- database/attempt-01.sql
+++ database/attempt-01.sql
@@ -9,19 +9,47 @@ DROP TABLE IF EXISTS Location;
 SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE Client (
-    id INT UNSIGNED AUTO NOT NULL,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) PRIMARY KEY
 );
 
 CREATE TABLE User (
-    client_id INT UNSIGNED NOT NULL,
+    client VARCHAR(255) NOT NULL,
     user_name VARCHAR(255) NOT NULL,
-    psw_hash_salted INT NOT NULL
+    psw_hash_salted INT NOT NULL,
+    PRIMARY KEY (client, user_name),
+    FOREIGN KEY (client) REFERENCES Client(name)
+);
+
+CREATE TABLE Vender (
+    -- id INT UNSIGNED AUTO NOT NULL,
+    poc VARCHAR(255),
+    email VARCHAR(255) PRIMARY KEY,
+    baa BOOL,
+    date DATE
+);
+
+CREATE TABLE Location (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    cloud_prem ENUM('Cloud', 'On-Premise'),
+    details VARCHAR(255), 
+    protection MEDIUMTEXT
+);
+
+CREATE TABLE Server (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    ip_address BIGINT UNSIGNED,
+    ip_version ENUM('IPv4', 'IPv6'),
+    location_id INT UNSIGNED NOT NULL,
+    CHECK (ip_address > 4294967295 OR ip_version='IPv4'),
+    UNIQUE (ip_address, ip_version),
+    FOREIGN KEY (location_id) REFERENCES Location(id)
 );
 
 -- break into smaller tables?
 CREATE TABLE Inv_Item (
-    client_id INT UNSIGNED NOT NULL,
+    item_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    client VARCHAR(255) NOT NULL,
     
     name VARCHAR(255),
     type VARCHAR(255),
@@ -29,19 +57,19 @@ CREATE TABLE Inv_Item (
     os VARCHAR(255),
     os_version VARCHAR(255),
     
-    vender_id INT UNSIGNED,
+    vender VARCHAR(255),
     auto_log_off_freq INT UNSIGNED,
-    server_id INT UNSIGNED,
+    server INT UNSIGNED,
 
     ephi BOOL,
     ephi_encrypted BOOL,
     ephi_encr_method VARCHAR(255),
     ephi_encr_tested BOOL,
-    interfaces_with _______, -- collection? references other Inv_Item entries? or varchar/MEDIUMTEXT text field?
+    interfaces_with MEDIUMTEXT, -- collection? references other Inv_Item entries? or varchar/MEDIUMTEXT text field?
 
     user_auth_method VARCHAR(255),
     app_auth_method VARCHAR(255),
-    psw_min_len INT,
+    psw_min_len INT UNSIGNED,
     psw_change_freq INT UNSIGNED,
 
     dept VARCHAR(255),
@@ -49,34 +77,14 @@ CREATE TABLE Inv_Item (
     date_last_ordered DATE,
     purchase_price DECIMAL(13, 4), -- GAP guideline
     warranty_expires DATE,
-    condition VARCHAR(255), -- larger? enum?
-    quantity INT,
+    item_condition VARCHAR(255), -- larger? enum?
+    quantity INT UNSIGNED,
     assset_value DECIMAL(13, 4),
     model_num VARCHAR(255),
     notes MEDIUMTEXT,
-    link VARCHAR(255)
-);
+    link VARCHAR(255),
 
-CREATE TABLE Vender (
-    id INT UNSIGNED AUTO NOT NULL,
-    poc VARCHAR(255),
-    email VARCHAR(255),
-    baa BOOL,
-    date DATE
-);
-
-CREATE TABLE Server (
-    id INT UNSIGNED AUTO NOT NULL,
-    name VARCHAR(255),
-    ip_address BIGINT UNSIGNED,
-    ip_version ENUM('IPv4', 'IPv6'),
-    location_id INT UNSIGNED,
-    CHECK (ip_address > 4294967295 OR ip_version='IPv4')
-);
-
-CREATE TABLE Location (
-    id INT UNSIGNED AUTO NOT NULL,
-    cloud_prem ENUM('Cloud', 'On-Premise'),
-    details VARCHAR(255), 
-    protection MEDIUMTEXT
+    FOREIGN KEY (client) REFERENCES Client(name),
+    FOREIGN KEY (vender) REFERENCES Vender(email),
+    FOREIGN KEY (server) REFERENCES Server(id)
 );
