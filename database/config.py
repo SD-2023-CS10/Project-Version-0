bb46b72c79a6b88cfe709fd0fa111847ce764e48
Brandon Huyck <bhuyck@pm.me>
2023-11-28 14:57:42 -0800

    add linux cmd for ease of dev

--- database/config.py
+++ database/config.py
@@ -6,6 +6,8 @@
 #     'db' : 'gu_devices'
 # }
 
+# mysql -h database-1.cjrnib4rikx5.us-west-2.rds.amazonaws.com -u gu_admin -peviTg8%qrtChcBC
+
 access_config = {
     'host' : 'database-1.cjrnib4rikx5.us-west-2.rds.amazonaws.com',
     'user' : 'gu_access',
