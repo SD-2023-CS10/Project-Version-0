c11944a7e9d6fce33a9a239e874de7be12c9068a
Colleen Lemak <clemak11@gmail.com>
2024-01-21 20:41:22 -0800

    MAC to int, OS parsed via pattern

--- crawler/crawl-device.py
+++ crawler/crawl-device.py
@@ -55,12 +55,25 @@ def get_hosts_up(subnet):
     for sent, received in result:
         mac = received.hwsrc # attribute holds parsed MAC address
         if mac != "":
-            macs_lst.append(mac)
+            mac_int = mac_to_int(mac)
+            macs_lst.append(mac_int)
         else:
             macs_lst.append("N/A")
     # Record hosts that respond to ARP requests
     hosts_up = [res[1].psrc for res in result]
     return hosts_up, macs_lst
+
+'''
+Function: mac_to_int()
+Args: mac_address
+Converts MAC address to integer
+Returns MAC address as an integer
+'''
+def mac_to_int(mac_address):
+    # Remove colons or dashes from the MAC address
+    cleaned_mac = ''.join(c for c in mac_address if c.isalnum())
+    mac_integer = int(cleaned_mac, 16)
+    return mac_integer
 
 '''
 Function: get_hostname()
@@ -85,7 +98,7 @@ Returns public IP of device running program.
 '''
 def get_public_ip():
     try:
-        response = requests.get('https://api64.ipify.org?format=json').json()
+        response = requests.get('https://api64.ipify.org?format=json', verify=True).json()
         public_ip = response["ip"]
     except Exception as e:
         print(f"Ensure administrator privileges and online connectivity. {e}")
@@ -100,7 +113,7 @@ Returns device's location attributes
 '''
 def get_location(server_ip):
     try:
-        response = requests.get(f'https://ipapi.co/{server_ip}/json/').json()
+        response = requests.get(f'https://ipapi.co/{server_ip}/json/', verify=True).json()
         city = response.get("city")
         region = response.get("region")
         country = response.get("country_name")
@@ -139,61 +152,99 @@ def append_NA(ports, protocols, statuses, services, service_versions):
     return ports, protocols, statuses, services, service_versions
 
 '''
+Function: split_os_details()
+Args: os_details
+Splits OS details into its corresponding name and version
+Returns parsed name and version
+'''
+def split_os_details(os_details):
+    # OS details is an re.Match object
+    print(f"DETAILS: {os_details}")
+    pattern = r"([a-zA-Z]+(?:\s+[a-zA-Z]+)*)\s*([\d. -]+)"
+    
+    if os_details:
+        os_str = str(os_details.group(1))
+        match = re.match(pattern, os_str)
+        os_name = match.group(1).strip()
+        print(os_name)
+        os_version = match.group(2).strip()
+        print(os_version)
+        return os_name, os_version
+    else:
+        return "N/A", "N/A"
+
+'''
+Function: find_ports()
+Args: nmap output, ports, protocols, statuses, services, service_versions
+Creates regex pattern to find ports in nmap output
+Returns: ports, protocols, statuses, services, service_versions
+'''
+def find_ports(output, ports, protocols, statuses, services, service_versions):
+    port_info_pattern = r"(\d+)/(\w+)\s+(\w+)\s+(.+?)\s*(?:\n|$)" # Parse nmap output to extract open ports and service versions
+    port_matches = re.findall(port_info_pattern, output)
+    if (len(port_matches) == 0):
+        append_NA(ports, protocols, statuses, services, service_versions)
+    for match in port_matches:
+        port = match[0] if match[0] != [] and match[0] != "" else "N/A"
+        ports.append(port)
+        protocol = match[1] if match[1] != [] and match[1] != "" else "N/A"
+        protocols.append(protocol)
+        status = match[2] if match[2] != [] and match[2] != "" else "N/A"
+        statuses.append(status)
+        split_string = match[3].split()
+
+        if len(split_string) >= 2:
+            split_string = [elem for elem in split_string if elem.strip()]
+            service = split_string[0]
+            version = ' '.join(split_string[1:])
+            print(f"Service: {service}, Version: {version}")
+            services.append(service.strip()) if service.strip() != "" else "N/A"
+            service_versions.append(version.strip()) if version.strip() != "" else service_versions.append("N/A")
+            print(f"Port: {port}, Protocol: {protocol}, Status: {status}, Service: {service.strip()}, Version: {version.strip()}")
+        else:
+            services.append("N/A")
+            service_versions.append("N/A")
+    return ports, protocols, statuses, services, service_versions
+
+'''
 Function: get_os_and_open_ports()
 Args: host IP
 Performs subprocess nmap scan for TCP fingerprinting of an OS
 Returns device_types, os_details, ports, protocols, statuses, services, service_versions
 '''
 def get_os_and_open_ports(host):
-    device_types = []; os_details = []; ports = []; protocols = []; statuses = []; services = []; service_versions = []
+    device_types = []; os_versions = []; os_names = []; ports = []; protocols = []; statuses = []; services = []; service_versions = []
     try:
         print("\n" + host)
         result = subprocess.run(['nmap', '-O', '-T4', '-sV', '-v', host], capture_output=True, text=True, timeout=190) # Run subprocess scan of nmap
         output = result.stdout
-        print(output)
         os_info_pattern = r"OS details: (.+)"
         os_match = re.search(os_info_pattern, output)
-        os_info = os_match.group(1) if os_match else "N/A"
-        os_details.append(os_info)
+        os_name, os_version = split_os_details(os_match)
+        os_names.append(os_name)
+        os_versions.append(os_version)
+        
+        print(f"Name: {os_name}")
+        print(f"Version: {os_version}")
+        
         device_pattern = r"Device type: (.+)" # Search for keywords in output
         device_match = re.search(device_pattern, output)
         device_info = device_match.group(1) if device_match else "N/A"
         device_types.append(device_info)
-        port_info_pattern = r"(\d+)/(\w+)\s+(\w+)\s+(.+?)\s*(?:\n|$)" # Parse nmap output to extract open ports and service versions
-        port_matches = re.findall(port_info_pattern, output)
-        if (len(port_matches) == 0):
-            append_NA(ports, protocols, statuses, services, service_versions)
-        for match in port_matches:
-            port = match[0] if match[0] != [] and match[0] != "" else "N/A"
-            ports.append(port)
-            protocol = match[1] if match[1] != [] and match[1] != "" else "N/A"
-            protocols.append(protocol)
-            status = match[2] if match[2] != [] and match[2] != "" else "N/A"
-            statuses.append(status)
-            split_string = match[3].split()
-
-            if len(split_string) >= 2:
-                split_string = [elem for elem in split_string if elem.strip()]
-                service = split_string[0]
-                version = ' '.join(split_string[1:])
-                print(f"Service: {service}, Version: {version}")
-                services.append(service.strip()) if service.strip() != "" else "N/A"
-                service_versions.append(version.strip()) if version.strip() != "" else service_versions.append("N/A")
-                print(f"Port: {port}, Protocol: {protocol}, Status: {status}, Service: {service.strip()}, Version: {version.strip()}")
-            else:
-                services.append("N/A")
-                service_versions.append("N/A")
+        ports, protocols, statuses, services, service_versions = find_ports(output, ports, protocols, statuses, services, service_versions)
     except subprocess.TimeoutExpired:
         print(f"Timeout occurred scanning {host}")
         device_types.append("N/A")
-        os_details.append("N/A")
+        os_names.append("N/A")
+        os_versions.append("N/A")
         append_NA(ports, protocols, statuses, services, service_versions)
     except Exception as e:
         print(f"Error retrieving OS information and open ports: {e}")
         device_types.append("N/A")
-        os_details.append("N/A")
+        os_names.append("N/A")
+        os_versions.append("N/A")
         append_NA(ports, protocols, statuses, services, service_versions)
-    return device_types, os_details, ports, protocols, statuses, services, service_versions
+    return device_types, os_names, os_versions, ports, protocols, statuses, services, service_versions
 
 '''
 Function: get_server_name()
@@ -257,18 +308,20 @@ Args: up_hosts, macs_lst, device_types, os_list, port_ids_lst, protocols_lst, st
 Prints lists to console
 No return value
 '''
-def print_summary(up_hosts, macs_lst, device_types, os_lst, port_ids_lst, protocols_lst, status_lst, services_lst, services_versions_lst):
+def print_summary(city, region, country, up_hosts, macs_lst, device_types, os_names, os_versions, port_ids_lst, protocols_lst, status_lst, services_lst, services_versions_lst):
     print("\nSummary:")
+    print(f"Location: {city}, {region}, {country}")
     for i in range(len(up_hosts)):
         print("Host: " + up_hosts[i])
-        print("MAC Address: " + macs_lst[i])
+        print("MAC Address: " + str(macs_lst[i]))
         print("Device Type: " + ", ".join(device_types[i]))
-        print("Operating System Name: " + ", ".join(os_lst[i]))
+        print("Operating System Name: " + ", ".join(os_names[i]))
+        print("Operating System Version: " + ", ".join(os_versions[i]))
         print("Port ID: " + ", ".join(port_ids_lst[i]))
-        print("Protocol: " + ", ".join(protocols_lst[i]))
-        print("Status: " + ", ".join(status_lst[i]))
-        print("Service: " + ", ".join(services_lst[i]))
-        print("Service Version: " + ", ".join(services_versions_lst[i])) 
+        print("Protocol: " + "".join(protocols_lst[i]))
+        print("Status: " + "".join(status_lst[i]))
+        print("Service: " + "".join(services_lst[i]))
+        print("Service Version: " + "".join(services_versions_lst[i]))
         print()
 
 '''
@@ -277,7 +330,7 @@ Args: up_hosts, device_types, os_lst, city, region, country, encryption, gateway
 Pushes crawled data to remote AWS server
 Returns no value
 '''
-def database_push(up_hosts, device_types, os_lst, city, region, country, encryption, gateway_ip, server_name):
+def database_push(up_hosts, device_types, os_names, os_versions, city, region, country, encryption, gateway_ip, server_name, services_versions_lst):
     # Add Project parent dir to path
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
@@ -290,27 +343,28 @@ def database_push(up_hosts, device_types, os_lst, city, region, country, encrypt
     with database as db:
         print("Pushing statistics to database...")
         try:
+            location = f"{city}, {region}, {country}"
+            location_id = db.create_locataion("On-Premise", location, encryption)
+            ip_obj = ipaddress.ip_address(gateway_ip)
+            ip_int = int(ip_obj)
+            server_id = db.create_server(server_name, ip_int, "IPv4", location_id)
+            
             for i in range(len(up_hosts)):
                 item_id = db.create_item()  # Inserting an item into the Inv_Item table
                 print("Adding Information for ID:", item_id)
                 db.set_name(up_hosts[i], item_id)  # hostname
                 db.set_type(", ".join(device_types[i]), item_id)  # device type
-                db.set_os(", ".join(os_lst[i]), item_id)  # os name
-                db.set_version("N/A", item_id)  # os version
-                
-                # db.set_mac(", ".join(macs_lst[i]), item_id)
-                # db.set_ports(", ".join(port_ids_lst[i]), item_id)
-                # db.set_protocols(", ".join(protocols_lst[i]), item_id)
-                # db.set_statuses(", ".join(status_lst[i]), item_id)
-                # db.set_services(", ".join(services_lst[i]), item_id)
-                # db.set_service_versions(", ".join(service_versions_lst[i]), item_id)
-            location = f"{city} , {region}, {country}"
-            location_id = db.create_locataion("On-Premise", location, encryption)
+                db.set_os(", ".join(os_names[i]), item_id)  # os name
+                db.set_version(", ".join(os_versions[i]), item_id)  # os version
+                db.set_server(server_id, item_id)
+                # Additional crawled attributes
+                db.set_mac(", ".join(macs_lst[i]), item_id)
+                db.set_ports(", ".join(port_ids_lst[i]), item_id)
+                db.set_protocols(", ".join(protocols_lst[i]), item_id)
+                db.set_statuses(", ".join(status_lst[i]), item_id)
+                db.set_services(", ".join(services_lst[i]), item_id)
+                db.set_services_versions(", ".join(services_versions_lst[i]), item_id)
             
-            ip_obj = ipaddress.ip_address(gateway_ip)
-            ip_int = int(ip_obj)
-            print(ip_int)
-            server_id = db.create_server(server_name, ip_int, "IPv4", location_id)
         except Exception as e:
             print("Error:", e)
             
@@ -320,13 +374,14 @@ Main Program Driver
 if __name__ == "__main__":
     print("Started Crawler...")
     nmap = Nmap() # Instantiate nmap object
-    device_types = []; os_lst = []; port_ids_lst = []; protocols_lst = []; status_lst = []; services_lst = []; services_versions_lst = []
+    device_types = []; os_names = []; os_versions = []; port_ids_lst = []; protocols_lst = []; status_lst = []; services_lst = []; services_versions_lst = []
     server_name, target_host, gateway_ip, subnet, up_hosts, macs_lst, num_devices, city, region, country, encryption = device_stats()
     for host in up_hosts:
         start_time = time.time()
-        device_type, os_details, ports, protocols, statuses, services, service_versions = get_os_and_open_ports(host)
+        device_type, os_name, os_version, ports, protocols, statuses, services, service_versions = get_os_and_open_ports(host)
         device_types.append(device_type)
-        os_lst.append(os_details)
+        os_names.append(os_name)
+        os_versions.append(os_version)
         port_ids_lst.append(ports)
         protocols_lst.append(protocols)
         status_lst.append(statuses)
@@ -334,6 +389,8 @@ if __name__ == "__main__":
         services_versions_lst.append(service_versions)
         end_time = time.time()
         print(f"Executed in {end_time - start_time} seconds.")
-    print_summary(up_hosts, macs_lst, device_types, os_lst, port_ids_lst, protocols_lst, status_lst, services_lst, services_versions_lst)
-    database_push(up_hosts, device_types, os_lst, city, region, country, encryption, gateway_ip, server_name)
+        
+    print_summary(city, region, country, up_hosts, macs_lst, device_types, os_names, os_versions, port_ids_lst, protocols_lst, status_lst, services_lst, services_versions_lst)
+    # TODO: Test database push and update auto-increment to sequentially increment IDs
+    # database_push(up_hosts, device_types, os_names, os_verisons, city, region, country, encryption, gateway_ip, server_name, services_versions_lst)
     
