aa397e57bccb378ca96119c076aa4f9082003710
Brandon Huyck <bhuyck@pm.me>
2024-02-08 12:09:54 -0800

    change dummy psw from int to vchar

--- database/dummy-data.sql
+++ database/dummy-data.sql
@@ -5,10 +5,10 @@ INSERT INTO Client VALUES
     ("MedTech");
 
 INSERT INTO User VALUES
-    ("MedCorp", "bhuyck1", 123),
-    ("MedCorp", "bhuyck2", 456),
-    ("GonMed", "bhuyck1", 123),
-    ("MedTech", "bhuyck1", 789);
+    ("MedCorp", "bhuyck1", "123"),
+    ("MedCorp", "bhuyck2", "456"),
+    ("GonMed", "bhuyck1", "123"),
+    ("MedTech", "bhuyck1", "789");
 
 INSERT INTO Vender VALUES
     ("Matt M.", "mattm@gmail.com", FALSE, '2023-10-31'),
