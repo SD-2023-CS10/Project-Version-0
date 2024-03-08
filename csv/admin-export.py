13c6778cc517231c359e23da78ae369dad98b62c
Brandon Huyck <bhuyck@pm.me>
2024-03-07 16:16:46 -0800

    header comment

--- csv/admin-export.py
+++ csv/admin-export.py
@@ -1,3 +1,34 @@
+'''
+ * File Name: admin-export.py
+ * 
+ * Description:
+ * This is the export script for Medcurity admins. It connects to the database, and then writes each
+ * row into a csv file for all of Medcurity's clients, attaching the client name to the row.
+ * Refer to /csv/README.md for more detailed information.
+ * 
+ * @package MedcurityNetworkScanner
+ * @authors Brandon Huyck (bhuyck@zagmail.gonzaga.edu)
+ * @license 
+ * @version 1.0.0
+ * @link 
+ * @since 
+ * 
+ * Usage:
+ * This file should be placed in the csv subdirectory of the application. It
+ * can be launched from its parent directory by invoking it with python. No
+ * modifications are necessary for basic operation, but customization can be
+ * done by editing the file paths.
+ * 
+ * Notes:
+ * - There are checks to ensure the person launching this is an admin; the UI checks when launhing there,
+ *   and the DBAPI checks that the username of the user it is signed in as is an admin.
+ * 
+ * TODO:
+ * - Update the header row writing to be done dynamially through the dbapi.py
+ *   admin_export() method.
+ * 
+'''
+
 import os, sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
