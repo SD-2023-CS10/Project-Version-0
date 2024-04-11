9a181a16437b4412c98487ce6c4785700441fdcc
Brandon Huyck <bhuyck@pm.me>
2024-04-11 12:25:35 -0700

    formatting

--- UI/addDevice.php
+++ UI/addDevice.php
@@ -30,36 +30,36 @@
  * 
  -->
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userDevice = $_POST["userDevice"];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $userDevice = $_POST["userDevice"];
 
-    $CLIENT = "Med INC";
+        $CLIENT = "Med INC";
 
-    // Add database connection and insertion logic here
-    $config = parse_ini_file("./config.ini");
-    $server = $config["servername"];
-    $username = $config["username"];
-    $password = $config["password"];
-    $database = "gu_devices";
+        // Add database connection and insertion logic here
+        $config = parse_ini_file("./config.ini");
+        $server = $config["servername"];
+        $username = $config["username"];
+        $password = $config["password"];
+        $database = "gu_devices";
 
-    $cn = mysqli_connect($server, $username, $password, $database);
+        $cn = mysqli_connect($server, $username, $password, $database);
 
-    // Connection error check
-    if (!$cn) {
-        die("Connection failed: " . mysqli_connect_error());
+        // Connection error check
+        if (!$cn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        // Create query and send
+        $insertQuery = "INSERT INTO Inv_Item (name, client) VALUES (?, ?)";
+        $st = $cn->prepare($insertQuery);
+        $st->bind_param("ss", $userDevice, $CLIENT);
+        $st->execute();
+
+        $st->close();
+        $cn->close();
+        
+        // Redirect back to the main page after adding the device
+        header("Location: index.php");
+        exit();
     }
-
-    // Create query and send
-    $insertQuery = "INSERT INTO Inv_Item (name, client) VALUES (?, ?)";
-    $st = $cn->prepare($insertQuery);
-    $st->bind_param("ss", $userDevice, $CLIENT);
-    $st->execute();
-
-    $st->close();
-    $cn->close();
-    
-    // Redirect back to the main page after adding the device
-    header("Location: index.php");
-    exit();
-}
 ?>
